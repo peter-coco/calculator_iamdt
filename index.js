3fb5e0301f52f50e13b94a6257837f9e6fdd27e0
peter-coco <dkliser@gmail.com>
2022-12-08 10:01:35 +0900

    [Feat] % 버튼에 대한 함수 div100 추가

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function init() {
         buttons.forEach(function (button) {
             button.addEventListener('click', onClickButton);
         });
-    if (calculatedResult)
-        calculatedResult.textContent = '0';
+    rendering(0);
 }
 /**
  * @description 계산기 버튼을 클릭하면 발생하는 함수. 어떤 버튼을 클릭했냐에 따라 로직이 분기됨.
@@ -46,6 +45,7 @@ function onClickButton(event) {
         return;
     }
     else if (textContent === '%') {
+        div100();
         console.log(calculateHistory);
         return;
     }
@@ -53,7 +53,7 @@ function onClickButton(event) {
     console.log(calculateHistory);
 }
 /**
- * @description 연산이나 숫자가 입력 및 변경됐을 때 화면을 랜더링하는 함수.
+ * @description 숫자가 입력 및 변경됐을 때 화면을 랜더링하는 함수.
  */
 function rendering(value) {
     var displayNum;
@@ -61,10 +61,11 @@ function rendering(value) {
         displayNum = value;
     }
     else {
-        displayNum = Number(value).toFixed(6);
+        // TODO : 실수 소수점의 자릿수가 커질 떄, 지수로 표현되면 안됨.
+        displayNum = Number(value);
     }
     if (calculatedResult && calculatedResult.textContent)
-        calculatedResult.textContent = "".concat(displayNum);
+        calculatedResult.textContent = "".concat(value);
 }
 /**
  * @description AC 버튼을 눌렀을때 작동하는 함수. calculateHistory를 초기화 하고, 0으로 랜더링한다.
@@ -81,7 +82,7 @@ function reset() {
 function calculate(mode) {
     switch (mode) {
         case '+':
-            //  temp_value를 초기화 하지 않는 이유는 calculate를 계속 눌렀을 때 동일한 피연산자로 동일한 연산을 해야하기 때문.
+            // NOTE : temp_value를 초기화 하지 않는 이유는 calculate를 계속 눌렀을 때 동일한 피연산자로 동일한 연산을 해야하기 때문.
             calculateHistory.value += calculateHistory.temp_value;
             rendering(calculateHistory.value);
             break;
@@ -99,6 +100,9 @@ function calculate(mode) {
             break;
     }
 }
+/**
+ * @description 두번째 피연산자가 있으면 그 값을 -1 없다면 첫번쨰 피연산자를 -1 곱해주는 함수.
+ */
 function changePositiveNegativeOfNumber() {
     if (calculateHistory.temp_value) {
         calculateHistory.temp_value *= -1;
@@ -109,6 +113,9 @@ function changePositiveNegativeOfNumber() {
         rendering(calculateHistory.value);
     }
 }
+/**
+ * @description cur_mode의 분기에 따라 display되는 숫자 로직 처리하는 함수.
+ */
 function addNumber(num) {
     if (calculatedResult && calculatedResult.textContent) {
         // 현재 사칙연산 모드가 선택된 경우
@@ -130,6 +137,9 @@ function addNumber(num) {
         }
     }
 }
+/**
+ * @description 현재 cur_mode의 분기에 따라 cur_mode를 추가하거나 calculate 함수를 호출한다.
+ */
 function addSubMulDiv(mode) {
     if (calculateHistory.cur_mode !== 'none') {
         // 연산 해줘야함.
@@ -137,6 +147,14 @@ function addSubMulDiv(mode) {
         return;
     }
     calculateHistory.cur_mode = mode;
+}
+/**
+ * @description calculateHistory.value를 100으로 나누고 rendering 함수 호출하는 함수.
+ */
+function div100() {
+    // TODO : %를 누른 상태에서 다른 숫자를 누르면 그냥 그 숫자로 대체 되야함.
+    calculateHistory.value /= 100;
+    rendering(calculateHistory.value);
 }
 // 초기화.
 init();
--- index.ts
+++ index.ts
@@ -12,7 +12,6 @@ const calculateHistory: calculateHistoryForm = {
   cur_mode: 'none',
   temp_value: 0,
 };
-
 /**
  * @description 웹페이지 접근시 이벤트 등록 및 화면을 초기화하는 함수.
  */
@@ -23,7 +22,7 @@ function init() {
       button.addEventListener('click', onClickButton);
     });
 
-  if (calculatedResult) calculatedResult.textContent = '0';
+  rendering(0);
 }
 
 /**
@@ -54,6 +53,7 @@ function onClickButton(event) {
     console.log(calculateHistory);
     return;
   } else if (textContent === '%') {
+    div100();
     console.log(calculateHistory);
     return;
   }
@@ -63,7 +63,7 @@ function onClickButton(event) {
 }
 
 /**
- * @description 연산이나 숫자가 입력 및 변경됐을 때 화면을 랜더링하는 함수.
+ * @description 숫자가 입력 및 변경됐을 때 화면을 랜더링하는 함수.
  */
 
 function rendering<T>(value: T) {
@@ -71,10 +71,10 @@ function rendering<T>(value: T) {
   if (Number.isInteger(Number(value))) {
     displayNum = value;
   } else {
-    displayNum = Number(value).toFixed(6);
+    // TODO : 실수 소수점의 자릿수가 커질 떄, 지수로 표현되면 안됨.
+    displayNum = Number(value);
   }
-  if (calculatedResult && calculatedResult.textContent)
-    calculatedResult.textContent = `${displayNum}`;
+  if (calculatedResult && calculatedResult.textContent) calculatedResult.textContent = `${value}`;
 }
 
 /**
@@ -95,7 +95,7 @@ function reset() {
 function calculate(mode: '+' | '-' | 'X' | '/') {
   switch (mode) {
     case '+':
-      //  temp_value를 초기화 하지 않는 이유는 calculate를 계속 눌렀을 때 동일한 피연산자로 동일한 연산을 해야하기 때문.
+      // NOTE : temp_value를 초기화 하지 않는 이유는 calculate를 계속 눌렀을 때 동일한 피연산자로 동일한 연산을 해야하기 때문.
       calculateHistory.value += calculateHistory.temp_value;
       rendering(calculateHistory.value);
       break;
@@ -114,6 +114,10 @@ function calculate(mode: '+' | '-' | 'X' | '/') {
   }
 }
 
+/**
+ * @description 두번째 피연산자가 있으면 그 값을 -1 없다면 첫번쨰 피연산자를 -1 곱해주는 함수.
+ */
+
 function changePositiveNegativeOfNumber() {
   if (calculateHistory.temp_value) {
     calculateHistory.temp_value *= -1;
@@ -123,6 +127,10 @@ function changePositiveNegativeOfNumber() {
     rendering(calculateHistory.value);
   }
 }
+
+/**
+ * @description cur_mode의 분기에 따라 display되는 숫자 로직 처리하는 함수.
+ */
 
 function addNumber(num: number) {
   if (calculatedResult && calculatedResult.textContent) {
@@ -145,6 +153,10 @@ function addNumber(num: number) {
   }
 }
 
+/**
+ * @description 현재 cur_mode의 분기에 따라 cur_mode를 추가하거나 calculate 함수를 호출한다.
+ */
+
 function addSubMulDiv(mode: '+' | '-' | 'X' | '/') {
   if (calculateHistory.cur_mode !== 'none') {
     // 연산 해줘야함.
@@ -153,6 +165,17 @@ function addSubMulDiv(mode: '+' | '-' | 'X' | '/') {
   }
 
   calculateHistory.cur_mode = mode;
+}
+
+/**
+ * @description calculateHistory.value를 100으로 나누고 rendering 함수 호출하는 함수.
+ */
+
+function div100() {
+  // TODO : %를 누른 상태에서 다른 숫자를 누르면 그냥 그 숫자로 대체 되야함.
+
+  calculateHistory.value /= 100;
+  rendering(calculateHistory.value);
 }
 
 // 초기화.
